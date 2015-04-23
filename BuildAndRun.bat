cc2143fbac6ff32f2c024df64b1ef4ea5755de45
vpeychev <vpeychev@RR-MAD-FJNTSJ1.CCClient.ConceptCapital.com>
2015-04-23 16:02:12 -0400

    Update BuildAndRun.bat

--- BuildAndRun.bat
+++ BuildAndRun.bat
@@ -1,3 +1,11 @@
+if exist "%programfiles%\Microsoft Visual Studio 14.0\Common7\Tools" (
+	call "%programfiles%\Microsoft Visual Studio 14.0\Common7\Tools\VSVARS32.BAT"
+) else (
+	if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools" (
+	call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VSVARS32.BAT"
+	) else goto end
+)
+
 REM Build solution
 msbuild /t:rebuild GCM.sln
 
