ce917a9093fa2f8f0d69a6489127f8ddc9764595
vpeychev <vpeychev@RR-MAD-FJNTSJ1.CCClient.ConceptCapital.com>
2015-04-23 12:15:18 -0400

    Update BuldAndRun.bat

--- BuildAndRun.bat
+++ BuildAndRun.bat
@@ -1,5 +1,5 @@
 REM Build solution
-@msbuild /t:rebuild GCM.sln
+msbuild /t:rebuild GCM.sln
 
-REM Run console application
-Solution\UI\ConsoleApp\bin\Debug\GCM.UI.exe
+REM Run test
+"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" UnitTestProject\bin\Debug\GCM.UnitTest.dll
